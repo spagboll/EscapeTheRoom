0d2aa77c91b5e247d420b8b4d8b3acd9a2fe9c39
Zach <zaachhodges@users.noreply.github.com>
2018-01-03 01:42:27 +0000

    code for extra room1 props

--- Assets/Scripts/Keypad.cs
+++ Assets/Scripts/Keypad.cs
@@ -5,17 +5,9 @@ using UnityStandardAssets.Characters.FirstPerson;
 
 public class Keypad : MonoBehaviour {
 
-//	public GameObject button1;
-//	public GameObject button2;
-//	public GameObject button3;
-//	public GameObject button4;
-//	public GameObject button5;
-//	public GameObject button6;
-//	public GameObject button7;
-//	public GameObject button8;
-//	public GameObject button9;
-//	public GameObject button0;
+
 	public GameObject player;
+	private string keyCode = "9999";
 
 	//public LayerMask interactLayer;
 
@@ -32,21 +24,17 @@ public class Keypad : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-//		if (Input.GetMouseButtonDown(0)) {
-//			RaycastHit hit;
-//			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-//			if (Physics.Raycast(ray, out hit)) {
-//				if (hit.transform.name == "Button0")
-//				{
-//				 Debug.Log ("Button0 pressed");
-//				}
-//			}
-//		}
+
 }
-		
+	//https://docs.unity3d.com/ScriptReference/RectTransform.html
 	public void CheckClick(RectTransform rectT){
 		
 		Debug.Log (rectT.name);
+	}
+
+	public void CheckInput(){
+	
+	
 	}
 }
 	
